f2760e8ee3c37ab1a83b8ab47b0800a1617032ac
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-08-24 22:34:21 -0300

    Criada a interface de domínio do cliente

---
+++ src/main/java/br/com/oficina/contrato/ICliente.java
@@ -0,0 +1,13 @@
+package br.com.oficina.contrato;
+
+import java.util.List;
+
+import br.com.oficina.model.Cliente;
+import br.com.oficina.service.NegocioException;
+
+public interface ICliente {
+
+	void saveOrUpdate(Cliente cliente) throws NegocioException;
+	void delete(Cliente cliente);
+	List<Cliente> getAll();
+}
