eba1510e2a3d888b6f1e3093bf0468740309eaa7
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-08-24 22:35:32 -0300

    Implementado o servie do cliente

---
+++ src/main/java/br/com/oficina/service/ClienteService.java
@@ -0,0 +1,37 @@
+package br.com.oficina.service;
+
+import java.util.List;
+
+import br.com.oficina.contrato.ICliente;
+import br.com.oficina.dao.ClienteDao;
+import br.com.oficina.model.Cliente;
+
+public class ClienteService implements ICliente {
+
+	private ClienteDao dao = new ClienteDao();
+	
+	@Override
+	public void saveOrUpdate(Cliente cliente) throws NegocioException {
+		if (cliente == null) {
+			throw new NegocioException("É necessário informar um cliente.");
+		}
+		
+		if (cliente.getNomeCompleto().equals("") || cliente.getNomeCompleto().trim().equalsIgnoreCase("")) {
+			throw new NegocioException("O nome do cliente não pode estar vazio");
+		}
+		
+		this.dao.saveOrUpdate(cliente);
+	}
+
+	@Override
+	public void delete(Cliente cliente) {
+		this.dao.delete(cliente);
+		
+	}
+
+	@Override
+	public List<Cliente> getAll() {
+		return this.dao.getAll();
+	}
+
+}
